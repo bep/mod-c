c47aa7ac4772f9142a3acc75544082d3013471a6
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-08-30 13:18:36 +0200

    Print b from c

--- p/lib.go
+++ p/lib.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"runtime/debug"
 	"strings"
+
+	mb "github.com/bep/mod-b/p"
 )
 
 func Version() string {
+	return "self: " + getVersion() + "\nmod-b: " + mb.Version() + "\n"
+}
+
+func getVersion() string {
 	bi, ok := debug.ReadBuildInfo()
 	if ok {
 		version := bi.Main.Version
